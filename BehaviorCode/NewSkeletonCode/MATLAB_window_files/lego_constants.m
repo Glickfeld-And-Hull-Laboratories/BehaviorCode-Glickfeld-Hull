c9295d0d60a51f4a867622e78fbedaae5f991d3d
Andrew McKinney <mckinney@neuro.duke.edu>
2014-06-13 13:16:29 -0400

    .mat files now saved

---
+++ BehaviorCode/NewSkeletonCode/MATLAB_window_files/lego_constants.m
@@ -0,0 +1,10 @@
+function consts = lego_constants
+
+tHostname = lower(hostname);
+tUsername = getenv('USER');
+homeDir = fullfile('/Users',tUsername);
+
+% defaults
+consts.centralDataPath = fullfile(homeDir, 'Documents/MWorks');
+consts.dataPath = fullfile(homeDir, 'Documents/MWorks/Data');
+consts.behavPdfPath = fullfile(homeDir, 'Documents/MWorks/BehavOutputPdfs');
