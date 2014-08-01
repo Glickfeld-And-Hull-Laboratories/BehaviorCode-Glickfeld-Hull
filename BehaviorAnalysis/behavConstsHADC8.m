4d567dc0b2e4bceb9408b97baf3d426faa21a482
Lindsey Glickfeld <llglick@gmail.com>
2014-08-01 17:32:51 -0400

    Path changes for Nuke

--- BehaviorAnalysis/behavConstsHADC8.m
+++ BehaviorAnalysis/behavConstsHADC8.m
@@ -1,36 +1,50 @@
 function rc = behavConstsHADC8
 
-rc.pathStr = '/Users/histed/data/mus-behavior/SnowLeopard/Data';
-rc.dataPat = 'data-i%03d-%s.mat';
 
-rootDir = '/Users/histed/analysis/mus-project/ramptrain-analysis1';
+tHostname = lower(hostname);
+[s,tUsername] = dos('ECHO %USERNAME%');
 
-rc.indexFilename = fullfile(rootDir, 'experimentIndexes/chr2-subj-days.xls');
-rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes/chr2-subj-fits.xls');
+switch tHostname
+    case {'zbook'}
+        rc.pathStr = '/Users/lindsey/Desktop/Data';
+        rc.dataPat = 'data-i%03d-%s.mat';
 
-rc.fitOutputTextCols = { 'DateStr', 'UniqueId', 'DateTimeStarted', 'PdfFigFilename', 'MatFilename' };
-rc.indexTextCols = { 'DateStr', 'UniqueId', 'DataBlock', 'TrialRangeToUse', 'Notes' };
+        rootDir = '/Users/lindsey/Documents/Behavior_data';
+        rc.indexFilename = fullfile(rootDir, 'experimentIndexes/chr2-subj-days-lg.xls');
+        rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes/chr2-subj-fits-lg.xls');
+        rc.fitOutputSummary = fullfile(rootDir, 'output/analysis/summary');
+        rc.fitOutputPdfDir = fullfile(rootDir, 'output/pdfFits');
+        rc.fitOutputMatDir = fullfile(rootDir, 'output/fitMatStats');
 
-rc.fitOutputPdfDir = fullfile(rootDir, 'output/pdfFits');
-rc.fitOutputMatDir = fullfile(rootDir, 'output/fitMatStats');
+    case {'nuke'}
+        if tUsername(1:7) == 'lindsey'
+            rc.pathStr = 'Z:\home\andrew\Behavior\Data';
+            rc.dataPat = 'data-i%03d-%s.mat';
+
+            rootDir = 'Z:\home\lindsey\Analysis\Behavior';
+            rc.indexFilename = fullfile(rootDir, 'experimentIndexes\subj-days-lg.xls');
+            rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes\subj-fits-lg.xls');
+            rc.fitOutputSummary = fullfile(rootDir, 'output\analysis\summary');
+            rc.fitOutputPdfDir = fullfile(rootDir, 'output\pdfFits');
+            rc.fitOutputMatDir = fullfile(rootDir, 'output\fitMatStats');
+        end
+end
+
+rc.fitOutputTextCols = { 'DateStr', 'DataBlock', 'DateTimeStarted', 'PdfFigFilename', 'MatFilename' };
+rc.indexTextCols = { 'DateStr', 'DataBlock', 'TrialRangeToUse', 'Notes' };
 
 rc.fhWeibull = @(p,xs) (p(4) + (p(3)-p(4)) .* (1 - exp( -(xs./p(1)) .^ p(2))));
 
-rc.trialOutcomes = { ...
-    'success', ...
-    'early', ...
-    'ignore', ...
-    'failure', ...
-    'incorrect', ...
-    'dualrelease' };
 
 %%%%%%%%%%% simple simple functions
 
 rc.computeFName = @subComputeFName;
 
-    function outFName = subComputeFName(subjNum, dateStr)
-        outFName = fullfile(rc.pathStr, sprintf(rc.dataPat, subjNum, deblank(dateStr)));
-    end
+function outFName = subComputeFName(subjNum, dateStr)
+    outFName = fullfile(rc.pathStr, sprintf(rc.dataPat, subjNum, deblank(dateStr)));
+end
+
+
 
 end
 
