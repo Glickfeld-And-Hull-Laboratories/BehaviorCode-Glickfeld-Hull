42b1d7732a60196b739d14f830141316f4fdab1e
Andrew McKinney <mckinney@neuro.duke.edu>
2014-11-12 11:35:58 -0500

    Added fitNormalToHolds for MATLAB analysis

---
+++ BehaviorAnalysis/fitNormalToHolds.m
@@ -0,0 +1,7 @@
+function [ pdf ] = fitNormalToHolds(subjNum, dateMat)
+%FITNORMALTOHOLDS: reads the hold times of a single day's RewardDelay data
+%and fits a normal distribution and plots relative to the onset of the
+%reward window.
+
+
+end
