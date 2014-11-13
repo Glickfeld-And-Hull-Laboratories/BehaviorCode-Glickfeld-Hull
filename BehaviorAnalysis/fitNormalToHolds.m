173aa05490f6a2a6f185763edb68db8b036f066c
Andrew McKinney <mckinney@neuro.duke.edu>
2014-11-13 13:17:29 -0500

    fitNormal work

--- BehaviorAnalysis/fitNormalToHolds.m
+++ BehaviorAnalysis/fitNormalToHolds.m
@@ -1,7 +1,15 @@
-function [ pdf ] = fitNormalToHolds(subjNum, dateMat)
+function [ pdf ] = fitNormalToHolds(ds)
 %FITNORMALTOHOLDS: reads the hold times of a single day's RewardDelay data
 %and fits a normal distribution and plots relative to the onset of the
 %reward window.
+if isfield(ds, 'tDelayTimeMs'),
+    expID = 'RewardDelay';
+end
+holdTimes = cell2mat_padded(ds.holdTimesMs);
+ht = sort(double(holdTimes));
+htMax = ceil(max(ht)/100)*100;
+bins = 0:100:htMax;
 
-
+[a] = histfit(ht, 50);
+mu = mean(a(2).XData)
 end
