df18e55fdffb7774c13aa29e8c94e962727080a5
Andrew McKinney <andmmckinney@gmail.com>
2014-12-12 16:36:54 -0500

    ReqXHolds analysis code

---
+++ BehaviorAnalysis/doAllReqXHolds.m
@@ -0,0 +1,45 @@
+function [dataStru outR] = doAllReqXHolds(subjMat, dateMat)
+global dataStru
+addpath('~/Repositories/BehaviorCode-Glickfeld-Hull/BehaviorAnalysis');
+% Data Path Formatting
+disp('Loading data from MWorks Data folder...')
+dataPath = '~/Documents/MWorks/Data';
+dirListing = dir(dataPath);
+fileNames= {dirListing.name};
+nSubjs= length(subjMat);
+    
+% Next line skips ".", "..", and ".DS_Store" hidden files. May not be applicable
+% in systems where hidden files are still hidden and should be removed if
+% it leads to data skippage.
+fileNames = fileNames(4:end);
+%% Data File Indexing and Pre-Processing
+outR = regexp(fileNames, 'data-i([0-9]*)-([0-9]*)-([0-9]*).mat', 'tokens');
+outRV = cat(1, outR{:});
+outRM = cat(1, outRV{:});
+outR
+global outR
+subjNumList = str2double(outRM(:,1));
+dateStrList = str2double(outRM(:,2));
+hourStrList = str2double(outRM(:,3));
+dataStru=struct;
+
+%% This section searches dataPath for files specific to each animal for the specified days, then loads specified parts of data into struData.
+for j = subjMat;
+    subjIx = subjNumList == j;
+    dateIx = ismember(dateStrList, dateMat);
+    subjDateIx = subjIx & dateIx;
+    dataStru(j).dates= dateMat;
+    desNames = fileNames(subjDateIx);
+    nNames = length(desNames);
+    for iN = 1:nNames;
+        tName = fullfile(dataPath, desNames{iN});
+        dataStru(j).check.downloadedname{1,iN}= tName; 
+        ds=mwLoadData(tName, 'max');
+        plotReqXHolds(ds)
+       disp('Daily Data Loaded')  
+    end
+    disp('Subject Data Loaded')
+end
+
+beep
+disp('Loading complete!')
---
+++ BehaviorAnalysis/plotReqXHolds.m
@@ -0,0 +1,26 @@
+function [] = plotReqXHolds( ds )
+% Used to plot x = hold times y = required hold time when input is a
+% behavior structure for HADC8.xml
+if ds.randReqHoldMaxMs > 5,
+    hts = cell2mat_padded(ds.holdTimesMs);
+    req = cell2mat_padded(ds.tTotalReqHoldTimeMs)+ds.tooFastTimeMs;
+    
+    [rewOnset, Ix] = sort(req);
+    rewEnd = cell2mat_padded(ds.tTotalReqHoldTimeMs)+ds.reactTimeMs;
+    rewEnd = rewEnd(Ix);
+    
+    [a] = scatter(req, hts);
+    jbfill(rewOnset, rewEnd, rewOnset, 'g', 'g', 1, 0.25)
+    ylabel('Hold Time (ms)');
+    xlabel('Required Hold Time (ms)');
+    grid on
+    title(ds.savedDataName(40:end));
+    outPath = '~/Desktop/HoldXReq/';
+    fName = strcat('reqXholds-', ds.savedDataName(40:55), '.pdf')
+    fileName = strcat(outPath, fName);
+    exportfig_print(gcf, fileName, 'FileFormat', 'pdf', ...
+             'Size', [12 9], ...
+             'PrintUI', false)
+    pdf = gcf;
+end
+end
