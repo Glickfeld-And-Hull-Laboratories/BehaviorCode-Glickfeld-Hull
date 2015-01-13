6d918e143e216916868d05de4c6743d58ab4f1d4
Andrew <mckinney@neuro.duke.edu>
2015-01-13 09:49:07 -0500

    dailyScript changes

--- BehaviorCode/NewSkeletonCode/Scripts/dailyScript.sh
+++ BehaviorCode/NewSkeletonCode/Scripts/dailyScript.sh
@@ -12,8 +12,8 @@ scp -r hullglick@hullglick4.local:~/Documents/MWorks/Data ~/Documents/MWorks
 scp -r hullglick@hullglick4.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
 
 mount -t smbfs //andrew:dukegrad@crash.dhe.duke.edu/andrew/behavior ~/Desktop/Crash_Server
-scp -r ~/Documents/MWorks/BehavOutputPdfs ~/Desktop/Crash_server/
-scp -r ~/Documents/MWorks/Data ~/Desktop/Crash_server/
+scp -r --exclude=.DS_Store ~/Documents/MWorks/BehavOutputPdfs ~/Desktop/Crash_server/
+scp -r --exclude=.DS_Store ~/Documents/MWorks/Data ~/Desktop/Crash_server/
 
 #/Applications/MATLAB_R2012b.app/bin/matlab -nosplash -nodesktop -r "dailyScript([212 213 214 305 306]);"
 #/Applications/MATLAB_R2012b.app/bin/matlab -nosplash -nodesktop -r "quit"
