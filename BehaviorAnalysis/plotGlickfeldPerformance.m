02a5f720313435912fc578e3b8b1ac400e272c05
Andrew McKinney <mckinney@neuro.duke.edu>
2015-03-18 18:06:11 -0400

    andrew analysis dump

--- BehaviorAnalysis/plotGlickfeldPerformance.m
+++ BehaviorAnalysis/plotGlickfeldPerformance.m
@@ -1,11 +1,12 @@
 function [x] = plotGlickfeldPerformance(subjMat)
+subjMat = [519]
 % Rewritten Starting Dec 2014 to automatically detect experiment type and load all dates in
 % the Data folder for a given mouse.
 global dataStru
 addpath('~/Repositories/BehaviorCode-Glickfeld-Hull/BehaviorAnalysis');
 % Data Path Formatting
 disp('Loading data from MWorks Data folder...')
-dataPath = '~/Documents/MWorks/Data';
+dataPath = 'C:\Users\andrew\Desktop\BehavData';
 dirListing = dir(dataPath);
 fileNames= {dirListing.name};
 nSubjs= length(subjMat);
@@ -21,7 +22,7 @@ outRM = cat(1, outRV{:});
 subjNumList = str2double(outRM(:,1));
 %dateStrList = str2double(outRM(:,2));
 %hourStrList = str2double(outRM(:,3));
-dataStru=struct;
+dataStruct=struct;
 
 %% This section searches dataPath for files specific to each animal for the specified days, then loads specified parts of data into struData.
 for j = subjMat;
@@ -30,91 +31,19 @@ for j = subjMat;
     nNames = length(desNames);
     for iN = 1:nNames;
         tName = fullfile(dataPath, desNames{iN});
-        dataStru(j).check.downloadedname{1,iN}= tName; 
+        dataStruct(j).check.downloadedname{1,iN}= tName; 
         ds=mwLoadData(tName, 'max');
         
-        %% hold time calculations
-%        rawHoldTimes = cell2mat(ds.holdTimesMs);
-%        medianHold = nanmedian(rawHoldTimes);
-%        meanHold = nanmean(rawHoldTimes);
-%        holdSTD = nanstd(double(rawHoldTimes));
-%        uSTD = meanHold+holdSTD;
-%%        tLSTD = meanHold-holdSTD;
-%        if (meanHold-holdSTD)>0,
-%          lSTD = tLSTD;
-%        else
-%          lSTD = 0;
-%        end
-        nTrials= length(ds.trialOutcomeCell);
-        disp(ds.savedDataName)
-        
-        %% replace this part with hadc8/flashingStim2/lego sorting code
-        if isfield(ds, 'tTotalReqHoldTimeMs'),
-            dataStru(j).values.isLego(1, iN) = 0;
-            %% do earlies analysis here
-            earlyIx = strcmp(ds.trialOutcomeCell, 'failure');
-            nEarly = sum(earlyIx);
-            dataStru(j).values.nEarlies(1,iN)= nEarly;
-            dataStru(j).values.perEarlies(1,iN)= nEarly/nTrials;
-            dataStru(j).values.react(1,iN)= ds.reactTimeMs;
-            
-            if isfield(ds, 'targetOnTimeMs')
-                dataStru(j).values.isLego(1, iN) = 0;
-                dataStru(j).values.isHADC8(1, iN) = 0;
-                dataStru(j).values.isFlashing(1, iN) = 1;
-            else
-                dataStru(j).values.isLego(1, iN) = 0;
-                dataStru(j).values.isHADC8(1, iN) = 1;
-                dataStru(j).values.isFlashing(1, iN) = 0;
-            end
-        elseif isfield(ds, 'delayTimeMs'),
-            dataStru(j).values.isLego(1, iN) = 1;
-            dataStru(j).values.isHADC8(1, iN) = 0;
-            dataStru(j).values.isFlashing(1, iN) = 0;
-            
-            dataStru(j).values.leftIx{1, iN} = ds.tLeftTrial;
-            
-%            dataStru(j).values.minHold(1,iN) = ds.delayTimeMs+ds.tooFastTimeMs;
-%            dataStru(j).values.maxHold(1,iN) = ds.delayTimeMs+ds.reactionTimeMs;
-            
-            %cancel out earlies analysis
-            dataStru(j).values.nEarlies(1,iN)= NaN;
-            dataStru(j).values.perEarlies(1,iN)= NaN;
-            
-            %% do separate incorrect analysis and avoid earlies
-            
-        end
-        
+        nTrials = length(ds.trialOutcomeCell);
+        dataStruct(j).values.nTrials = nTrials;
         %% trial outcome calculations
         corrIx = strcmp(ds.trialOutcomeCell, 'success');
-        nCorrect = sum(corrIx);
+        dataStruct(j).values.perCorrect{1,iN} = sum(corrIx)/nTrials;
         igIx = strcmp(ds.trialOutcomeCell, 'ignore');
-        nLate = sum(igIx);
-       % corrHolds = rawHoldTimes(corrIx);
-        
-        perCorrect= nCorrect/nTrials;
-        perLate= nLate/nTrials;
-        %perEarly= nEarly/nTrials;
+        dataStruct(j).values.perIgnore{1,iN} = sum(igIx)/nTrials;
         incIx = strcmp(ds.trialOutcomeCell, 'incorrect');
-        perInc= sum(incIx)/nTrials;
-        dataStru(j).values.perInc(1,iN)= perInc;
+        dataStruct(j).values.perIncorrect{1,iN} = sum(incIx)/nTrials;
         
-%        dataStru(j).values.correctHoldSD(1, iN) = nanstd(double(corrHolds));
-%        dataStru(j).values.correctHoldMean(1, iN) = nanmean(corrHolds);
-        
-        dataStru(j).values.nCorrects(1,iN)= nCorrect;
-        dataStru(j).values.nLates(1,iN)= nLate;
-        %dataStru(j).values.medianHold(1,iN)= medianHold;
-        %dataStru(j).values.meanHold(1,iN)= meanHold;
-        %dataStru(j).values.stdHold(1,iN)= holdSTD;
-        %dataStru(j).values.uSTD(1,iN)= uSTD;
-        %dataStru(j).values.lSTD(1,iN)= lSTD;
-        dataStru(j).values.perCorrects(1,iN)= perCorrect;
-        %dataStru(j).values.perEarlies(1,iN)= perEarly;
-        dataStru(j).values.perLates(1,iN)= perLate;
-        dataStru(j).values.ITI(1, iN)= ds.itiTimeMs;
-        dataStru(j).values.nTrials(1, iN)= nTrials;
-%        dataStru(j).values.holdTimesCell{iN} = histc(double(cell2mat(ds.holdTimesMs)), 0:100:5000);
     end
     disp(strcat('Subject Data from i', num2str(j), ' Loaded'))
 end
@@ -127,26 +56,24 @@ disp('Loading complete!')
 for i=1:length(subjMat),
     subPlotSz = {3,1};
     sub = subjMat(i);
-    v = dataStru(sub).values;
-    pCorr = v.perCorrects;
-    pEarly = v.perEarlies;
-    pLate = v.perLates; 
-    pInc = v.perInc;
+    v = dataStruct(519).values;
+    pCorr = cell2mat(v.perCorrect);
+ %   pEarly = v.perEarlies;
+    pLate = cell2mat(v.perIgnore); 
+    pInc = cell2mat(v.perIncorrect);
 %    medH = v.medianHold;
 %    uSTD = v.uSTD;
 %    lSTD = v.lSTD;
-    nTrials = v.nTrials;
-    nCorr = v.nCorrects;
-    nDays = length(v.perCorrects);
+    nDays = length(v.perCorrect);
     xDays = 1:nDays;
 
 %%  
 
-    axH = subplot(subPlotSz{:}, 1);
+    axH = subplot(subPlotSz{:}, 1:3);
     hold on
-    try
+
     plot(pCorr, '-kv', 'LineWidth', 2);
-    plot(pEarly, '-cv', 'LineWidth', 2);
+   % plot(pEarly, '-cv', 'LineWidth', 2);
     plot(pLate, '-mv', 'LineWidth', 2);
     plot(pInc, '-gv', 'LineWidth', 2);
     ylabel('Percent');
@@ -156,52 +83,18 @@ for i=1:length(subjMat),
     xlim([1 nDays])
     ylim([0 1]);
     %legend('Correct', 'Early', 'Late', 'No Release', 'Location', 'Best')
-    catch
+
         disp('Fig 1 failed');
-    end;
-    grid on; 
-%%
-    axH = subplot(subPlotSz{:}, 2);
-    hold on
-    grid on; 
-    
-    try
-    lIx = cell2mat(v.leftIx);
-    LCorr = pCorr(lIx);
-    LLate = pLate(lIx); 
-    LInc = pInc(lIx);
-    
-    plot(LCorr, '-kv', 'LineWidth', 2);
-    plot(LLate, '-mv', 'LineWidth', 2);
-    plot(LInc, '-gv', 'LineWidth', 2);
-    ylabel('Left Percent');
-    xlabel('Training Day');
-    
-%%    
-    rIx = ~lIx;
-    RCorr = pCorr(rIx);
-    RLate = pLate(rIx); 
-    RInc = pInc(rIx);
-    
-    plot(RCorr, '-kv', 'LineWidth', 2);
-    plot(RLate, '-mv', 'LineWidth', 2);
-    plot(RInc, '-gv', 'LineWidth', 2);
-    ylabel('Right Percent');
-    xlabel('Training Day');
-    
-    catch
-        disp('Fig 2 Failed')
-    end
     
     fName = strcat('Performance -- i', num2str(sub), ' -- Generated:', datestr(today, 'dd mmmm yyyy'));
     set(gcf, 'Name', fName);
     
     
-    sName = strcat('~/Documents/MWorks/DailyPlots/plotPerformance-i', num2str(sub),'-', datestr(today, 'yymmdd'), '.pdf');
+    sName = strcat('C:\Users\andrew\Desktop\BehavData\', num2str(sub),'-', datestr(today, 'yymmdd'), '.pdf');
     epParams = { gcf, sName, ...
              'FileFormat', 'pdf', ...
              'Size', [12 12], ...
              'PrintUI', false };
          exportfig_print(epParams{:});
-         close
+end
 end
--- BehaviorAnalysis/plotReqXHolds.m
+++ BehaviorAnalysis/plotReqXHolds.m
@@ -1,27 +1,54 @@
-function [] = plotReqXHolds( ds )
+%function [ds] = plotReqXHolds( ds )
 % Used to plot x = hold times y = required hold time when input is a
 % behavior structure for HADC8.xml
 disp(isfield(ds, 'randReqHoldMaxMs'))
 if isfield(ds, 'randReqHoldMaxMs'),
     hts = cell2mat_padded(ds.holdTimesMs);
-    req = cell2mat_padded(ds.tTotalReqHoldTimeMs)+ds.tooFastTimeMs;
+    hts = double(hts);
+    req = double(cell2mat_padded(ds.tTotalReqHoldTimeMs))+double(ds.tooFastTimeMs);
     
     [rewOnset, Ix] = sort(req);
     rewEnd = cell2mat_padded(ds.tTotalReqHoldTimeMs)+ds.reactTimeMs;
-    rewEnd = rewEnd(Ix);    
+    rewEnd = rewEnd(Ix);
+    corrIx = hts>req;
     
-    [a] = scatter(req, hts);
+    subplotSize = {1,2};
+    axH = subplot(subplotSize{:}, 1)
+    eHT = hts(~corrIx);
+    totalHTs = length(eHT);
+    [a b] = hist(eHT, ceil(range(eHT)/10));
+    probs = a/totalHTs;
+    cdfStuff = cumsum(probs);
+    plot(cdfStuff,b)
+    
+    %pH1 = cdfplot(hts(~corrIx));
+    %set(gca,'YTickLabel',[]);
+    %set(gca,'XTickLabel',[]);
+    xlabel('F(x)')
+    ylabel('Hold Time of Early Releases(ms)')
+    axis tight
+    grid on
+    ylim([min(hts) max(hts)])
+    axH = subplot(subplotSize{:}, 2)
+    pH2 = scatter(req(~corrIx), hts(~corrIx), 'b');
+    %[a] = scatterhist(req(~corrIx), hts(~corrIx), 'Direction', 'out', 'NBins', [20 ceil(range(hts(~corrIx))/100)], 'Color', 'b');
+    hold on
+    pH3 = scatter(req(corrIx), hts(corrIx), 'k');
+    if range(req)>50,
+        xlim([min(req) max(req)]);
+    end
+    ylim([min(hts) max(hts)]);
     jbfill(rewOnset, rewEnd, rewOnset, 'g', 'g', 1, 0.25)
     ylabel('Hold Time (ms)');
     xlabel('Required Hold Time (ms)');
     grid on
     title(ds.savedDataName(40:end));
-    outPath = '~/Desktop/HoldXReq/';
-    fName = strcat('reqXholds-', ds.savedDataName(40:55), '.pdf')
+    outPath = 'C:\Users\andrew\Desktop\HoldXReq\';
+    fName = strcat('reqXholds2-', ds.savedDataName(40:55), '.pdf')
     fileName = strcat(outPath, fName);
     exportfig_print(gcf, fileName, 'FileFormat', 'pdf', ...
              'Size', [12 9], ...
              'PrintUI', false)
     pdf = gcf;
 end
-end
+%end
