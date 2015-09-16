5734e7c6a5530f0e4036e8ec8e911584791e449f
Lindsey Glickfeld <llglick@gmail.com>
2015-09-16 18:59:39 -0400

    path changes for eyetracking - LG

--- BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
+++ BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
@@ -2,17 +2,28 @@ function rc = behavConstsAV
 
 
 tHostname = lower(hostname);
+[s,tUsername] = dos('ECHO %USERNAME%');
 
 switch tHostname
     case {'nuke'}
         rc.pathStr = '\\CRASH.dhe.duke.edu\data\home\andrew\Behavior\Data';
         rc.dataPat = 'data-i%03d-%s.mat';
-
-        rootDir = '\\CRASH.dhe.duke.edu\data\home\lindsey\Analysis\Behavior';
-        rc.indexFilename = fullfile(rootDir, 'experimentIndexes\av-subj-days-lg.xls');
-        rc.fitOutputSummary = fullfile(rootDir, 'output\summary');
-        rc.fitOutputPdfDir = fullfile(rootDir, 'output\pdfFits');
-        rc.fitOutputMatDir = fullfile(rootDir, 'output\fitMatStats');
+        
+        if tUsername(1:7) == 'lindsey'
+            rootDir = '\\CRASH.dhe.duke.edu\data\home\lindsey\Analysis\Behavior';
+            rc.indexFilename = fullfile(rootDir, 'experimentIndexes\av-subj-days-lg.xls');
+            rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes\av-subj-fits-lg.xls');
+            rc.fitOutputSummary = fullfile(rootDir, 'output\summary');
+            rc.fitOutputPdfDir = fullfile(rootDir, 'output\pdfFits');
+            rc.fitOutputMatDir = fullfile(rootDir, 'output\fitMatStats');
+            rc.eyeOutputDir = fullfile(rootDir,'EyeTracking');
+        elseif tUsername(1:7) == 'ashley'
+            rc.indexFilename = fullfile(rootDir, 'experimentIndexes\subj-days-lg.xls');
+            rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes\subj-fits-lg.xls');
+            rc.fitOutputSummary = fullfile(rootDir, 'output\analysis\summary');
+            rc.fitOutputPdfDir = fullfile(rootDir, 'output\pdfFits');
+            rc.fitOutputMatDir = fullfile(rootDir, 'output\fitMatStats');
+        end
 end
 
 rc.fitOutputTextCols = { 'DateStr', 'DataBlock', 'DateTimeStarted', 'PdfFigFilename', 'MatFilename' };
