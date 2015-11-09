4430de3d5ea869f134b22b4f5363c5b11a5f99e3
Andrew McKinney <mckinney@neuro.duke.edu>
2015-11-09 10:24:16 -0500

    not pushed yet?-AW

--- BehaviorCode/NewSkeletonCode/Scripts/pushToCrash.sh
+++ BehaviorCode/NewSkeletonCode/Scripts/pushToCrash.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mount -t smbfs //andrew:dukegrad@crash.dhe.duke.edu/andrew/behavior ~/Desktop/Crash_Server
 rsync --ignore-existing -rv ~/Documents/MWorks/BehavOutputPdfs ~/Desktop/Crash_server/
 rsync --ignore-existing -rv ~/Documents/MWorks/Data ~/Desktop/Crash_server/
