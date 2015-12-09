ff7f87bc845e7ab6862abeb5986632b21297231e
ashley <wilson@neuro.duke.edu>
2015-12-09 13:34:18 -0500

    getting code up to date - AW

--- BehaviorAnalysis/AW analysis/behavParamsAV_AW.m
+++ BehaviorAnalysis/AW analysis/behavParamsAV_AW.m
@@ -4,8 +4,5 @@ function av = behavParamsAV
     av(3).mouse = 616;
     av(1).col_str = 'm';
     av(2).col_str = 'b';
-    av(3).col_str = 'k';
-    av(1).sec_col_str = 'r';
-    av(2).sec_col_str = 'c';
-    av(3).sec_col_str = 'y';
+    av(3).col_str = 'r';
 end
--- BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
+++ BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
@@ -23,14 +23,15 @@ switch tHostname
             rc.ashleyAnalysis = '\\CRASH.dhe.duke.edu\data\home\ashley\analysis';
         elseif tUsername(1:5) == 'ashle'
             rc.name = 'ashley';
-            rootDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis\Behavior';
-            rc.indexFilename = fullfile(rootDir, 'experimentIndexes\av-subj-days-lg.xls');
-            rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes\av-subj-fits-lg.xls');
-            rc.fitOutputSummary = fullfile(rootDir, 'output\analysis\summary');
-            rc.fitOutputPdfDir = fullfile(rootDir, 'output\pdfFits');
-            rc.fitOutputMatDir = fullfile(rootDir, 'output\fitMatStats');
-            rc.eyeOutputDir = fullfile(rootDir,'Summaries\eye tracking');
+            rootDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis';
+            rc.indexFilename = fullfile(rootDir, '\Behavior\experimentIndexes\av-subj-days-lg.xls');
+            rc.fitOutputFilename = fullfile(rootDir, '\Behavior\experimentIndexes\av-subj-fits-lg.xls');
+            rc.fitOutputSummary = fullfile(rootDir, '\Behavior\output\analysis\summary');
+            rc.fitOutputPdfDir = fullfile(rootDir, '\Behavior\output\pdfFits');
+            rc.fitOutputMatDir = fullfile(rootDir, '\Behavior\output\fitMatStats');
+            rc.eyeOutputDir = fullfile(rootDir);
             rc.eyeInputDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis\';
+            rc.caOutputDir = fullfile(rootDir);
        elseif tUsername(1:5) == 'aiwei'
    
             rc.name = 'aiwei';
