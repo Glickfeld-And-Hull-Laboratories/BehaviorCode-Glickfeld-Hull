260b7b17f795911efed37b78b498aa7fe3694ef9
Ashley <wilson@neuro.duke.edu>
2016-01-25 16:13:06 -0500

    change plotting time   -ZX

--- BehaviorCode/NewSkeletonCode/MATLAB_window_files/plotCerebellarStim.m
+++ BehaviorCode/NewSkeletonCode/MATLAB_window_files/plotCerebellarStim.m
@@ -3,6 +3,7 @@ function input = plotCerebellarStim(data_struct, input)
 % essential consts
 figNum = 1;
 name = 'CerebellarStim';
+smoothType = 'moving';
 
 % set up environment
 tHostname = lower(hostname);
@@ -28,14 +29,17 @@ wheelTime = input.quadratureTimesUs;
 emptyIndex = cellfun(@isempty,wheelTime);       %# Find indices of empty cells
 wheelTime(emptyIndex) = {int64(0)};                    %# Fill empty cells with 0
 %mylogicalarray = logical(cell2mat(mycellarray));  %# Convert the cell array
-wheelTime = diff(cell2mat(wheelTime))/1000;     % convert time to ms
+wheelTime_diff = diff(cell2mat(wheelTime))/1000;     % convert time to ms
 
-wheelSpeed  = double(wheelIx)./double(wheelTime);
-avgWheelN   = 100;
+wheelSpeed  = double(wheelIx)./double(wheelTime_diff);
+avgWheelN   = 10;
 
-if length(wheelSpeed) >= 100
+if length(wheelSpeed) >= 10
+    WheelTimePoint  = cell2mat(wheelTime);
+    WheelTimePoint  = WheelTimePoint(1:avgWheelN:end)/1e6; 
     meanWheelSpeed  = arrayfun(@(i) mean(wheelSpeed(i:i+avgWheelN-1)),1:avgWheelN:length(wheelSpeed)-avgWheelN+1);
-    plot(meanWheelSpeed);
+    plot(WheelTimePoint(1:length(meanWheelSpeed)),smooth(WheelTimePoint(1:length(meanWheelSpeed)),meanWheelSpeed,smoothType));
+    %plot(meanWheelSpeed);
 % lH = plot(double(wheelIx), nTrial);
 % set(lH, 'Color', 'r', ...
 %         'LineWidth', 3);
@@ -43,14 +47,14 @@ if length(wheelSpeed) >= 100
 % set(lH2, 'Color', 'k', ...
 %         'LineWidth', 2);
         
-    title('Mean Running rate every 1 second')
-    ylabel('Running Rate');
-    xlabel('Second')
-else
-    plot(wheelSpeed);
-    title('Mean Running rate for about 10 milisecond')
-    ylabel('Running Rate');
-    xlabel('Second')
+    title('Mean Running rate every 10 movements registered')
+    ylabel('Running Rate (pulses/ms)');
+    xlabel('Timestamp (s)')
+% else
+%     plot(wheelSpeed);
+%     title('Mean Running rate for every 1 movement registered')
+%     ylabel('Running Rate');
+%     %xlabel('Second')
 end
 
 %% Add a save button
