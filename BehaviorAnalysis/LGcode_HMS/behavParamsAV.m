0d30d3f4628dcf8c1b76c42e449cd1372fb858fb
ashley <wilson@neuro.duke.edu>
2016-02-21 09:57:31 -0500

    mouse added - AW

--- BehaviorAnalysis/LGcode_HMS/behavParamsAV.m
+++ BehaviorAnalysis/LGcode_HMS/behavParamsAV.m
@@ -2,10 +2,13 @@ function av = behavParamsAV
     av(1).mouse = 613;
     av(2).mouse = 614;
     av(3).mouse = 616;
+    av(4).mouse = 626;
     av(1).col_str = 'm';
     av(2).col_str = 'b';
     av(3).col_str = 'k';
+    av(4).col_str = 'c';
     av(1).sec_col_str = 'r';
     av(2).sec_col_str = 'c';
     av(3).sec_col_str = 'y';
+    av(4).sec_col_str = 'r';
 end
