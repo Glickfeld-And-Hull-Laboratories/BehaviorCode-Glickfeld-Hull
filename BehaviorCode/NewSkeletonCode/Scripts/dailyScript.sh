92dfa63a78e35cd6cffafe0177600424ce9625c6
ziye xu <dec.ziye@gmail.com>
2016-06-27 15:20:54 -0400

    Change dailyScript     -ZX

--- BehaviorCode/NewSkeletonCode/Scripts/dailyScript.sh
+++ BehaviorCode/NewSkeletonCode/Scripts/dailyScript.sh
@@ -2,22 +2,29 @@
 #  dailyScript.sh
 #  
 # Depends on RSA handshake between HullGlickN computers.
-#  Created by Andrew McKinney on 11/8/2014.
+#  Created by Andrew McKinney on 11/12/13.
 #
-scp -r hullglick@hullglick2.local:~/Documents/MWorks/Data ~/Documents/MWorks
-scp -r hullglick@hullglick2.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
-scp -r hullglick@hullglick3.local:~/Documents/MWorks/Data ~/Documents/MWorks
-scp -r hullglick@hullglick3.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
-scp -r hullglick@hullglick4.local:~/Documents/MWorks/Data ~/Documents/MWorks
-scp -r hullglick@hullglick4.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
+
+#sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick2.local:~/Documents/MWorks/Data ~/Documents/MWorks 
+#sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick2.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
+#sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick3.local:~/Documents/MWorks/Data ~/Documents/MWorks
+#sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick3.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
+#sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick4.local:~/Documents/MWorks/Data ~/Documents/MWorks
+#sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick4.local:~/Documents/MWorks/BehavOutputPdfs ~/Documents/MWorks
 
 mount -t smbfs //andrew:dukegrad@crash.dhe.duke.edu/andrew/behavior ~/Desktop/Crash_Server
-scp -r --exclude=.DS_Store ~/Documents/MWorks/BehavOutputPdfs ~/Desktop/Crash_server/
-scp -r --exclude=.DS_Store ~/Documents/MWorks/Data ~/Desktop/Crash_server/
+for num in 1 2 3 4
+do
+    sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick$num.local:~/Documents/MWorks/Data ~/Desktop/Crash_server/ 
+    sshpass -p hullglick rsync --ignore-existing -rv hullglick@hullglick$num.local:~/Documents/MWorks/BehavOutputPdfs ~/Desktop/Crash_server/
+done
 
-#/Applications/MATLAB_R2012b.app/bin/matlab -nosplash -nodesktop -r "dailyScript([212 213 214 225 226 305 306]);"
+#sshpass -p hullglick rsync --ignore-existing -rv ~/Documents/MWorks/BehavOutputPdfs ~/Desktop/Crash_server/
+#sshpass -p hullglick rsync --ignore-existing -rv ~/Documents/MWorks/Data ~/Desktop/Crash_server/
+
+#/Applications/MATLAB_R2012b.app/bin/matlab -nosplash -nodesktop -r "dailyScript([212 213 214 305 306]);"
 #/Applications/MATLAB_R2012b.app/bin/matlab -nosplash -nodesktop -r "quit"
-#scp -r ~/Documents/MWorks/DailyPlots ~/Desktop/Crash_server/
+#rsync -rv ~/Documents/MWorks/DailyPlots ~/Desktop/Crash_server/
 
 umount ~/Desktop/Crash_Server
 
