df949a5440c028e8e50cb764c42c83fb13847172
ziye <dec.ziye@gmail.com>
2016-12-20 12:45:27 -0500

    Added pre block trials for 2AFC randProb- LG

--- BehaviorCode/NewSkeletonCode/calcRandProb.py
+++ BehaviorCode/NewSkeletonCode/calcRandProb.py
@@ -8,24 +8,56 @@ def calc_tStimProbAvgLeft():
 	compTrials = getvar('tNTrialsCompleted')
 	nTrialPerBlock = getvar('RandProbTrialperBlock')
 	lastIndex = getvar('tRandProbIndexLast')
+	preblock = getvar('RandProbPreBlockTrials')
 
 	if compTrials == 0:
 		if 0.5 in probList:
 			ind = probList.index(0.5)
 			setvar('tStimProbAvgLeft', 0.5)
 			setvar('tRandProbIndexLast', ind)
+			setvar('inPreBlock', 0)
 		else:
 			tempval = random.randint(1,numProbs)-1
-			setvar('tStimProbAvgLeft', probList[tempval])
 			setvar('tRandProbIndexLast', tempval)
-	elif compTrials%nTrialPerBlock > 0:
-		print lastIndex
-		setvar('tStimProbAvgLeft', probList[lastIndex])
+			if preblock > 0:
+				setvar('inPreBlock', 1)
+				if probList[tempval] < 0.5:
+					setvar('tStimProbAvgLeft', 0)
+				if probList[tempval] > 0.5:
+					setvar('tStimProbAvgLeft', 1)
+			elif preblock == 0:
+				setvar('inPreBlock', 0)
+				setvar('tStimProbAvgLeft', probList[tempval])
 	elif compTrials%nTrialPerBlock == 0:
 		tempval = lastIndex
 		while(tempval == lastIndex):
 			tempval = random.randint(1,numProbs)-1
-		setvar('tStimProbAvgLeft', probList[tempval])
-		setvar('tRandProbIndexLast', tempval)
+			setvar('tRandProbIndexLast', tempval)
+		if preblock > 0:
+			setvar('inPreBlock', 1)
+			if probList[tempval] < 0.5:
+				setvar('tStimProbAvgLeft', 0)
+			if probList[tempval] > 0.5:
+				setvar('tStimProbAvgLeft', 1)
+		elif preblock == 0:
+			setvar('inPreBlock', 0)
+			setvar('tStimProbAvgLeft', probList[tempval])
+	elif preblock>0:
+		if compTrials%nTrialPerBlock < preblock+1:
+			setvar('inPreBlock', 1)
+			setvar('tRandProbIndexLast', lastIndex)
+			if probList[lastIndex] < 0.5:
+				setvar('tStimProbAvgLeft', 0)
+			if probList[lastIndex] > 0.5:
+				setvar('tStimProbAvgLeft', 1)
+		if compTrials%nTrialPerBlock > preblock:
+			setvar('inPreBlock', 0)	
+			setvar('tStimProbAvgLeft', probList[lastIndex])
+			setvar('tRandProbIndexLast', lastIndex)
+	elif preblock == 0:
+		setvar('inPreBlock', 0)		
+		if compTrials%nTrialPerBlock > 0:
+			setvar('tStimProbAvgLeft', probList[lastIndex])
+			setvar('tRandProbIndexLast', lastIndex)
 
 
