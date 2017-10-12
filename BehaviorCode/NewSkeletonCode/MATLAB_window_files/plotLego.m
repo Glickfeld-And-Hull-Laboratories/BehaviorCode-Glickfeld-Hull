9e949b11450cfea4dc5d0da9cc95561a81925cf5
ziye <dec.ziye@gmail.com>
2017-10-12 13:22:58 -0400

    Fixed plotting issue for size comparison- LG

--- BehaviorCode/NewSkeletonCode/MATLAB_window_files/plotLego.m
+++ BehaviorCode/NewSkeletonCode/MATLAB_window_files/plotLego.m
@@ -835,28 +835,28 @@ hold on
 if isfield(input,'doContrastDiscrim')
   if input.doContrastDiscrim
     if input.gratingContrastDiffSPO > 10
-      differenceRight = chop(cell2mat(input.rightGratingContrast) - cell2mat(input.leftGratingContrast),2);
+      differenceRight = chop(celleqel2mat_padded(input.rightGratingContrast) - celleqel2mat_padded(input.leftGratingContrast),2);
     elseif ~isfield(input, 'dGratingContrastDiff') & input.gratingContrastDiffSPO <= 10
-      differenceRight = chop(cell2mat(input.rightGratingContrast) - cell2mat(input.leftGratingContrast),2);
+      differenceRight = chop(celleqel2mat_padded(input.rightGratingContrast) - celleqel2mat_padded(input.leftGratingContrast),2);
     elseif isfield(input, 'dGratingContrastDiff') & input.gratingContrastDiffSPO <= 10
-      differenceRight =chop(cell2mat(input.rightGratingContrast) ./ cell2mat(input.leftGratingContrast),2);
+      differenceRight =chop(celleqel2mat_padded(input.rightGratingContrast) ./ celleqel2mat_padded(input.leftGratingContrast),2);
     end
   elseif input.doSizeDiscrim
   if input.gratingDiameterDiffSPO > 10
-      differenceRight = chop(cell2mat(input.rightGratingDiameterDeg) - cell2mat(input.leftGratingDiameterDeg),2);
+      differenceRight = chop(celleqel2mat_padded(input.rightGratingDiameterDeg) - celleqel2mat_padded(input.leftGratingDiameterDeg),2);
     elseif ~isfield(input, 'dGratingDiameterDiff') & input.gratingDiameterDiffSPO <= 10
-      differenceRight = chop(cell2mat(input.rightGratingDiameterDeg) - cell2mat(input.leftGratingDiameterDeg),2);
+      differenceRight = chop(celleqel2mat_padded(input.rightGratingDiameterDeg) - celleqel2mat_padded(input.leftGratingDiameterDeg),2);
     elseif isfield(input, 'dGratingDiameterDiff') & input.gratingDiameterDiffSPO <= 10
-      differenceRight =chop(cell2mat(input.rightGratingDiameterDeg) ./ cell2mat(input.leftGratingDiameterDeg),2);
+        differenceRight =chop(celleqel2mat_padded(input.rightGratingDiameterDeg) ./ celleqel2mat_padded(input.leftGratingDiameterDeg),2);
     end
   end
 else
   if input.gratingContrastDiffSPO > 10
-    differenceRight = chop(cell2mat(input.rightGratingContrast) - cell2mat(input.leftGratingContrast),2);
+    differenceRight = chop(celleqel2mat_padded(input.rightGratingContrast) - celleqel2mat_padded(input.leftGratingContrast),2);
   elseif ~isfield(input, 'dGratingContrastDiff') & input.gratingContrastDiffSPO <= 10
-    differenceRight = chop(cell2mat(input.rightGratingContrast) - cell2mat(input.leftGratingContrast),2);
+    differenceRight = chop(celleqel2mat_padded(input.rightGratingContrast) - celleqel2mat_padded(input.leftGratingContrast),2);
   elseif isfield(input, 'dGratingContrastDiff') & input.gratingContrastDiffSPO <= 10
-    differenceRight =chop(cell2mat(input.rightGratingContrast) ./ cell2mat(input.leftGratingContrast),2);
+    differenceRight =chop(celleqel2mat_padded(input.rightGratingContrast) ./ celleqel2mat_padded(input.leftGratingContrast),2);
   end
 end
 
