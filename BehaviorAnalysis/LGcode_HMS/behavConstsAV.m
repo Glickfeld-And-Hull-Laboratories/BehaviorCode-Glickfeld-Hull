64d223e26cfde6a0e2e619f4ebb5b780446e10ea
ashley <wilson@neuro.duke.edu>
2018-03-21 13:36:25 -0400

    added new analysis path

--- BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
+++ BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
@@ -37,10 +37,11 @@ switch tHostname
             rc.eyeInputDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis\';
             rc.caOutputDir = fullfile(rootDir, 'FSAV Summaries');
             rc.ashleyAnalysis = fullfile(rootDir);
+            rc.ashleyData = '\\CRASH.dhe.duke.edu\data\home\ashley\data';
         elseif tUsername(1:5) == 'xiaor'
-            rc.name = 'ashle';
+            rc.name = 'xiaor';
             rootDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis';
-            rc.behavData = 'Y:\home\andrew\Behavior\Data';
+            rc.behavData = '\\CRASH.dhe.duke.edu\data\home\andrew\Behavior\Data';
             rc.indexFilename = fullfile(rootDir, '\Behavior\experimentIndexes\av-subj-days-lg.xls');
             rc.indexFilename_audCtrl = fullfile(rootDir, '\Behavior\experimentIndexes\v-subj-days-lg.xls');
             rc.indexFilename_fsavImg = fullfile(rootDir, '\Behavior\experimentIndexes\fsav-img-subj-days-lg.xls');
@@ -52,18 +53,8 @@ switch tHostname
             rc.eyeOutputDir = fullfile(rootDir, 'FSAV Summaries','pupil');
             rc.eyeInputDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis\';
             rc.caOutputDir = fullfile(rootDir, 'FSAV Summaries');
-            rc.ashleyAnalysis = fullfile(rootDir);
-       elseif tUsername(1:5) == 'aiwei'
-   
-            rc.name = 'aiwei';
-            rootDir = '\\CRASH.dhe.duke.edu\data\home\aiwei\Analysis\Behavior_data';
-            rc.indexFilename = fullfile(rootDir, 'experimentIndexes\av-subj-days-lg.xls');
-            rc.fitOutputFilename = fullfile(rootDir, 'experimentIndexes\av-subj-fits-lg.xls');
-            rc.fitOutputSummary = fullfile(rootDir, 'output\analysis\summary');
-            rc.fitOutputPdfDir = fullfile(rootDir, 'output\pdfFits');
-            rc.fitOutputMatDir = fullfile(rootDir, 'output\fitMatStats');
-
-     
+            rc.ashleyAnalysis = fullfile(rootDir);     
+            rc.ashleyData = '\\CRASH.dhe.duke.edu\data\home\ashley\data';
         end
 end
 
