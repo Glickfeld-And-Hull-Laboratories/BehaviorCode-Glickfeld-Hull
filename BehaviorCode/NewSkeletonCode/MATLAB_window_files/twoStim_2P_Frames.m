92f30b449bf00eca2e5622f5f7f7fc409e3a1d2b
ziye <dec.ziye@gmail.com>
2018-04-23 12:17:48 -0400

    edits for two stim matlab output- LG

--- BehaviorCode/NewSkeletonCode/MATLAB_window_files/twoStim_2P_Frames.m
+++ BehaviorCode/NewSkeletonCode/MATLAB_window_files/twoStim_2P_Frames.m
@@ -7,19 +7,19 @@ function [retval] = HoldAndDetectConstant(data_struct, input)
 
 if nargin < 2,
     input = [];
-    input.saveTime = datestr(now, 'yymmdd-HHMM'); 
 end
 
 ds = data_struct;
 addpath('~/Repositories/BehaviorCode-Glickfeld-Hull/BehaviorCode/NewSkeletonCode/MatlabSharedCode');
 addpath('~/Repositories/BehaviorCode-Glickfeld-Hull/BehaviorAnalysis');
 
-varsOneValEachTrial = {...
+oneValEachTrialNames = {...
     'tTrialStartMWTimestampMs', ...
     'tItiWaitTimeMs', ...
     'tThisTrialStartTimeMs', ...
     'tLastTrialStartTimeMs', ...
-    'tTempStimOdds', ...
+    'tTempStim1', ...
+    'tTempStim2', ...
     'tStimulusNumber', ...
     'tStimulusNumber2', ...
     'tStimOneGratingOnTimeMs', ...
@@ -57,8 +57,6 @@ varsOneValEachTrial = {...
     'nStimTwoFramesOn', ...
     'nFramesISI', ...
     'cItiStart',...
-    'cLeverDown',...
-    'cFirstStim',...
     'cStimOneOn',...
     'cStimTwoOn',...
     'cStimOneOff',...
@@ -69,10 +67,82 @@ varsOneValEachTrial = {...
     'mwStimTwoOffMs',...
 };
 
-exptSetupBridge;
+events = ds.events;
+eventsTrial = events;
 
-[input eventsConsts eventsTrial] ...
-    = exptProcessBridgeInput(data_struct, input, varsOneValEachTrial);
+if isempty(input),
+    input.trialSinceReset = 1;
+    input.startDateVec = datevec(now);
+    input.saveTime = datestr(now, 'yymmdd-HHMM');
+    input.savedEvents = {};
+    input.eventCodecs = {};
+    input.eventCodecs{1} = ds.event_codec;
+    nOne = length(oneValEachTrialNames);
+    for iV = 1:nOne
+        input.(oneValEachTrialNames{iV}) = {};
+    end
+else
+    input.trialSinceReset = input.trialSinceReset+1;
+end
+trN = input.trialSinceReset;
+
+% Consts that govern tValues
+input.constList = { 'subjectNum',...
+    'experimentXmlTrialId', ...
+    'speedIntervalUS', ...
+    'doMatrix'};
+
+for iV = 1:length(oneValEachTrialNames)
+    tName = oneValEachTrialNames{iV};
+    input.(tName){trN} = mwGetEventValue(eventsTrial, ds.event_codec, tName, 'last', 1);
+end
+
+nSync = length(mwGetEventValue(ds.events, ds.event_codec, ...
+                             'sync', 'all', 'ignoreMissing'));
+
+                         
+syncCode = codec_tag2code(ds.event_codec, 'sync');
+codeList = [ds.events.event_code];
+syncNs = find(codeList == syncCode);                        
+if nSync >= 4,
+  % if 6 codes, pull out consts from first block and leave last
+  % (Somewhat dangerous)
+  constsStruct = struct;
+  constsStruct = ds.events(1:syncNs(3));
+  trialStruct = ds.events(syncNs(end-1):end);
+  ds.events = trialStruct;
+end
+
+
+nConsts = length(input.constList);
+for iC = 1:nConsts
+  tCN = input.constList{iC};
+  if input.trialSinceReset==1  % use dump on first trial
+    tCV1 = mwGetEventValue(constsStruct, ds.event_codec, tCN, [], ...
+                          'ignoremissing');
+    input.(tCN) = tCV1;
+  
+  else  % look for changes on all trials
+    tCV = mwGetEventValue(ds.events, ds.event_codec, tCN, [], ...
+                          'ignoremissing');
+    if ~isempty(tCV)
+      input.(tCN) = tCV;
+    end
+  end
+end
+ 
+% Look for changes in constants for all trials
+b={};
+for iC = 1:nConsts
+  tCN = input.constList{iC};
+  tV = mwGetEventValue(ds.events, ds.event_codec, tCN, [], 'ignoremissing');
+ 
+  if ~isempty(tV), 
+    % Constructs array of changed consts and when they changed
+    b(end+1,1:2) = {tCN, tV};
+  end
+end
+input.constChangedOnTrial{input.trialSinceReset} = b;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
