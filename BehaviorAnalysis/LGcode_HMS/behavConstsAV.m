f5e06f92239f3e48fa5e6754314747aa1f628514
Ashley <wilson@neuro.duke.edu>
2018-10-17 11:33:57 -0400

    added CD hostname for real this time

--- BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
+++ BehaviorAnalysis/LGcode_HMS/behavConstsAV.m
@@ -42,21 +42,10 @@ switch tHostname
             rc.ashleyAnalysis = fullfile(rootDirIsilon,'Analysis');
             rc.ashleyData = fullfile(rootDirIsilon,'data');
 %             rc.isilon = rootDirIsilon;
-        elseif tUsername(1:5) == 'xiaor'
-            rc.name = 'xiaor';
-            rootDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis';
+        elseif tUsername(1:5) == 'carol'
+            rc.name = 'carolyn';
+            rootDir = '\\duhs-user-nc1.dhe.duke.edu\dusom_glickfeldlab\all_staff\home\carolyn';
             rc.behavData = '\\CRASH.dhe.duke.edu\data\home\andrew\Behavior\Data';
-            rc.indexFilename = fullfile(rootDir, '\Behavior\experimentIndexes\av-subj-days-lg.xls');
-            rc.indexFilename_audCtrl = fullfile(rootDir, '\Behavior\experimentIndexes\v-subj-days-lg.xls');
-            rc.indexFilename_fsavImg = fullfile(rootDir, '\Behavior\experimentIndexes\fsav-img-subj-days-lg.xls');
-            rc.eaMouseIndexFilename = fullfile(rootDir,'Behavior','experimentIndexes','FSAV_eaMouseData.xls');
-            rc.fitOutputFilename = fullfile(rootDir, '\Behavior\experimentIndexes\av-subj-fits-lg.xls');
-            rc.fitOutputSummary = fullfile(rootDir, '\Behavior\output\analysis\summary');
-            rc.fitOutputPdfDir = fullfile(rootDir, '\Behavior\output\pdfFits');
-            rc.fitOutputMatDir = fullfile(rootDir, '\Behavior\output\fitMatStats');
-            rc.eyeOutputDir = fullfile(rootDir, 'FSAV Summaries','pupil');
-            rc.eyeInputDir = '\\CRASH.dhe.duke.edu\data\home\ashley\Analysis\';
-            rc.caOutputDir = fullfile(rootDir, 'FSAV Summaries');
             rc.ashleyAnalysis = fullfile(rootDir);     
             rc.ashleyData = '\\CRASH.dhe.duke.edu\data\home\ashley\data';
         end
