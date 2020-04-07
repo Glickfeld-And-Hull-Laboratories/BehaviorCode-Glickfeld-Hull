1865dbfedb53a35a19720cfddc6aa390a421621c
Lindsey Glickfeld <llglick@gmail.com>
2020-04-07 18:02:11 -0400

    mouse list- LG

---
+++ BehaviorAnalysis/LGcode_Duke/exptLists/contrastDiscrimMouseList.m
@@ -0,0 +1,12 @@
+mouse = [];
+%% i441
+mouse(1).name = 'i441';
+mouse(1).genotype = 'PV';
+mouse(1).opsin = 'Arch';
+mouse(1).task = 'ContrastDiscrim';
+
+%% i460
+mouse(2).name= 'i460';
+mouse(2).genotype = 'SOM';
+mouse(2).opsin = 'Arch';
+mouse(2).task = 'ContrastDiscrim';
