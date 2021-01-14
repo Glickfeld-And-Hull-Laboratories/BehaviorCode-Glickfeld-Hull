8d42236f0a1972a538e3d2eefef23d4f7591356b
Lindsey Glickfeld <llglick@gmail.com>
2021-01-14 12:54:09 -0500

    fixed selecCalc for Ori- LG

--- BehaviorAnalysis/LGcode_Duke/selectCalc.m
+++ BehaviorAnalysis/LGcode_Duke/selectCalc.m
@@ -12,19 +12,30 @@ function [s, b] = selectCalc(input,trials)
             if isempty(find(unique(rCon)==0))
                 ratio_mat = chop(rCon./lCon,3);
                 doRatio = 1;
+                doOriDiscrim = 0;
+                doContrastDetect = 0;
             else
                 ratio_mat = rCon-lCon;
                 doRatio = 0;
+                doContrastDetect = 1;
+                doOriDiscrim =0;
             end
+             tLeftTrial = celleqel2mat_padded(input.tLeftTrial);
         elseif input.doSizeDiscrim
             rSize = celleqel2mat_padded(input.rightGratingDiameterDeg);
             lSize = celleqel2mat_padded(input.leftGratingDiameterDeg);
             ratio_mat = chop(rSize./lSize,3);
             doRatio = 1;
+            tLeftTrial = celleqel2mat_padded(input.tLeftTrial);
+            doOriDiscrim = 0;
+            doContrastDetect = 0;
         elseif input.doOriDiscrim
             ratio_mat = -1.*celleqel2mat_padded(input.tGratingDirectionStart);
             doRatio = 0;
-        end
+            doOriDiscrim = 1;
+            doContrastDetect = 0;
+            tLeftTrial = ~celleqel2mat_padded(input.tLeftTrial);
+        end    
     elseif isfield(input, 'rightGratingContrast')
         rCon = celleqel2mat_padded(input.rightGratingContrast);
         lCon = celleqel2mat_padded(input.leftGratingContrast);
@@ -55,15 +66,12 @@ function [s, b] = selectCalc(input,trials)
         end
     end
     ratios = unique(ratio_mat);
-    if isfield(input,'tRightResponse')
-        tRight = celleqel2mat_padded(input.tRightResponse);
-    else
-        SIx = strcmp(input.trialOutcomeCell,'success');
-        MIx = strcmp(input.trialOutcomeCell,'incorrect');
-        tRight = zeros(size(rCon));
-        tRight(intersect(find(~tLeftTrial),find(SIx))) = 1;
-        tRight(intersect(find(tLeftTrial),find(MIx))) = 1;
-    end
+    
+    SIx = strcmp(input.trialOutcomeCell,'success');
+    MIx = strcmp(input.trialOutcomeCell,'incorrect');
+    tRight = zeros(size(ratio_mat));
+    tRight(intersect(find(~tLeftTrial),find(SIx))) = 1;
+    tRight(intersect(find(tLeftTrial),find(MIx))) = 1;
     if isfield(input,'doBlock2')
         if input.doBlock2
             b2ind = find(celleqel2mat_padded(input.tBlock2TrialNumber));
@@ -75,23 +83,41 @@ function [s, b] = selectCalc(input,trials)
         ratio_mat(indNogo) = 0;
         tRight(indNogo) = 0;
     end
+    if isfield(input,'doAdapt')
+        indAdapt = find(celleqel2mat_padded(input.aGratingContrast));
+        ratio_mat(indAdapt) = 0;
+    end
     Iind = find(strcmp(input.trialOutcomeCell,'ignore'));
     ratio_mat(Iind) = 0;
-    totR = sum(ratio_mat==ratios(end));
-    pctRonR = sum(ratio_mat==ratios(end) & tRight)./totR;
-    totL = sum(ratio_mat==ratios(1));
-    pctRonL = sum(ratio_mat==ratios(1) & tRight)./totL;
-    s = pctRonR-pctRonL;
+    
     if doRatio
+        totR = sum(ratio_mat==ratios(end));
+        pctRonR = sum(ratio_mat==ratios(end) & tRight)./totR;
+        totL = sum(ratio_mat==ratios(1));
+        pctRonL = sum(ratio_mat==ratios(1) & tRight)./totL;
         totR_all = sum(ratio_mat>1);
         totL_all = sum(ratio_mat<1 & ratio_mat>0);
         pctRonR_all = sum(ratio_mat>1 & tRight)./totR_all;
         pctRonL_all = sum(ratio_mat<1 & ratio_mat>0 & tRight)./totL_all;
-    else
+    elseif doContrastDetect
+        totR = sum(ratio_mat==ratios(end));
+        pctRonR = sum(ratio_mat==ratios(end) & tRight)./totR;
+        totL = sum(ratio_mat==ratios(1));
+        pctRonL = sum(ratio_mat==ratios(1) & tRight)./totL;
         totR_all = sum(ratio_mat>0);
         totL_all = sum(ratio_mat<0);
         pctRonR_all = sum(ratio_mat>0 & tRight)./totR_all;
         pctRonL_all = sum(ratio_mat<0 & tRight)./totL_all;
+    elseif doOriDiscrim
+        totR = sum(ratio_mat==ratios(1));
+        pctRonR = sum(ratio_mat==ratios(1) & tRight)./totR;
+        totL = sum(ratio_mat==ratios(end));
+        pctRonL = sum(ratio_mat==ratios(end) & tRight)./totL;
+        totR_all = sum(ratio_mat<0);
+        totL_all = sum(ratio_mat>0);
+        pctRonR_all = sum(ratio_mat<0 & tRight)./totR_all;
+        pctRonL_all = sum(ratio_mat>0 & tRight)./totL_all;
     end
+    s = pctRonR-pctRonL;
     b = ((pctRonR_all+pctRonL_all)./2)-0.5;
 end
