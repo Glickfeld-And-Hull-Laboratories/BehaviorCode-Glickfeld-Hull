1a68dd4a64d7dc3fd60cbb4d09d9886da0746e07
ziye <dec.ziye@gmail.com>
2021-02-03 14:02:27 -0500

    code for turning laser on and off during Hold and Detect 2P frames

---
+++ BehaviorCode/NewSkeletonCode/L0.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = "10.122.170.98"
+UDP_PORT = 7000
+MESSAGE = b"L0"
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
