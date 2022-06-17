8a7296f5bbfdd5738c757ec93224247b366250ba
Lan <test>
2022-06-17 11:36:38 -0400

    push from 2p computer

--- BehaviorCode/Old-maunsell/scripts/experimentSaveDefaults.py
+++ BehaviorCode/Old-maunsell/scripts/experimentSaveDefaults.py
@@ -20,7 +20,7 @@ import re
 domainName = "org.mworks-project.MWClient"
 
 
-outFile = os.path.expanduser(os.path.join('~/Repositories/BehaviorCode-Glickfeld-Hull/BehaviorCode/NewSkeletonCode/Scripts/Plists', 'org.Behavior.Lego_Frames.plist'))
+outFile = os.path.expanduser(os.path.join('~/Repositories/BehaviorCode-Glickfeld-Hull/BehaviorCode/NewSkeletonCode/Scripts/Plists', 'org.Behavior.VisStimRet.plist'))
 keyNames = [ 
   'MATLAB client window - selected variables',
   'MATLAB client window - MATLAB .m file',
