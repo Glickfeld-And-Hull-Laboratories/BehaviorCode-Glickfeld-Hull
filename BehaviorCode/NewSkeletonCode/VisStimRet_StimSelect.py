89a6c22e07efcf260af6d399c385c8513cd17ff0
LindseyG <llglick@gmail.com>
2024-04-30 17:43:18 -0400

    Testing new stimulus selection variables- LG

---
+++ BehaviorCode/NewSkeletonCode/VisStimRet_StimSelect.py
@@ -0,0 +1,28 @@
+import math
+import numpy
+import random
+
+if tTrialsDoneSinceStart == 0:
+	usedList = []
+else:
+	usedList = getvar('usedList')
+
+nStimCond = getvar('nStimCond')
+stimList = list(range(0,nStimCond*5-1))
+
+unUsedList = list(set(usedList).symmetric_difference(set(stimList)))
+if len(unUsedList)==0:
+	unUsedList = stimList
+	usedList = []
+
+doRand = getvar('doRand')
+if doRand == 1:
+	selectStim = random.choice(unUsedList)
+else:
+	selectStim = unUsedList(0)
+
+usedList.append(selectStim)
+
+setvar('usedList',usedList)
+setvar('tStimulusNumber',selectStim)
+
